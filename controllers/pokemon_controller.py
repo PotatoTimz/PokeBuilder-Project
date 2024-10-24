049b0ec78fe17949200f2ef0ac35fd16d6e5707c
Andre Alix <andrealix2698@gmail.com>
2024-10-24 15:23:42 -0400

    minor changes to pokemon controller/services

--- controllers/pokemon_controller.py
+++ controllers/pokemon_controller.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, abort, request, jsonify
 from services.jwt_token_service import token_required
-from services.pokemon_service import get_all_pokemons, add_pokemon, validate_data
+from services.pokemon_service import delete_pokemon, update_pokemon, is_created_by_user, get_pokemon_by_id, get_all_pokemons, add_pokemon, validate_data, get_pokemon_by_user
 
 pokemon_bp = Blueprint("pokemon_bp", __name__)
 
+# Get all pokemon in the database
 @pokemon_bp.route("/pokemon", methods=["GET"])
 def manage_pokemon():
     if request.method == "GET":
@@ -15,8 +16,41 @@ def manage_pokemon():
                             "types": pokemon[3],
                             "image": pokemon[4]
                         } for pokemon in pokemons])
+    
+# Get pokemon from id
+@pokemon_bp.route("/pokemon/<int:pokemon_id>", methods=["GET"])
+def manage_by_id_pokemon(pokemon_id):
+    if request.method == "GET":
+        pokemon = get_pokemon_by_id(pokemon_id)
+        return jsonify({ 
+                            "id": pokemon[0], 
+                            "name": pokemon[1],
+                            "creator": pokemon[2],
+                            "types": pokemon[3],
+                            "image": pokemon[4]
+                        })
+    
+# Update and Delete Pokemon
+@pokemon_bp.route("/pokemon/<int:pokemon_id>", methods=["PUT", "DELETE"])
+@token_required
+def modify_delete_pokemon(user_data, pokemon_id):
+    if request.method == "PUT":
+        if not is_created_by_user(pokemon_id, user_data.username):
+            abort(400, "You cannot edit a pokemon you didn't create")
 
-@pokemon_bp.route("/pokemon", methods=["POST", "GET"])
+        data = request.json
+        types, name, image = validate_data(data)
+
+        update_pokemon(types, name, image, pokemon_id)
+
+        return jsonify({"message": "pokemon successfully updated!"})
+    if request.method == "DELETE":
+        delete_pokemon(pokemon_id)
+        return jsonify({"message": "pokemon successfully deleted!"})
+
+
+# Create pokemon, Get current users created pokemon
+@pokemon_bp.route("/user/pokemon", methods=["POST", "GET"])
 @token_required
 def manage_user_pokemon(user_data):
     if request.method == "POST":
@@ -28,3 +62,26 @@ def manage_user_pokemon(user_data):
         add_pokemon(types, name, image, user_data.username)
 
         return jsonify({"message": "pokemon successfully added!"})
+    
+    if request.method == "GET":
+        pokemons = get_pokemon_by_user(user_data.username)
+        return jsonify([{
+                            "id": pokemon[0], 
+                            "name": pokemon[1],
+                            "creator": pokemon[2],
+                            "types": pokemon[3],
+                            "image": pokemon[4]
+                        } for pokemon in pokemons])
+
+# Get pokemon from other users
+@pokemon_bp.route("/user/pokemon/<int:user_id>", methods=["GET"])
+def manage_other_user_pokemon(user_id):
+    if request.method == "GET":
+        pokemons = get_pokemon_by_user(user_id)
+        return jsonify([{
+                            "id": pokemon[0], 
+                            "name": pokemon[1],
+                            "creator": pokemon[2],
+                            "types": pokemon[3],
+                            "image": pokemon[4]
+                        } for pokemon in pokemons])
--- services/pokemon_service.py
+++ services/pokemon_service.py
@@ -6,7 +6,7 @@ from models.account import Account
 from services.type_service import valid_type
 
 from extensions import db
-from flask import request, jsonify, current_app, abort
+from flask import abort
 from sqlalchemy import func
 
 def get_all_pokemons():
@@ -16,6 +16,27 @@ def get_all_pokemons():
               .join(PokemonType)
               .join(Type)
               .group_by(Pokemon.name).all())
+
+def get_pokemon_by_id(id):
+    return (db.session.query(Pokemon, Account, PokemonType, Type)
+        .with_entities(Pokemon.id, Pokemon.name, Account.username.label("creator"), func.group_concat(Type.name).label("types"), Pokemon.image)
+        .join(Account)
+        .join(PokemonType)
+        .join(Type)
+        .filter(Pokemon.id == id)
+        .group_by(Pokemon.name).first())
+
+def get_pokemon_by_user(account_id):
+    if isinstance(account_id, str):
+        account_id = Account.query.filter_by(username=account_id).first().id
+    
+    return (db.session.query(Pokemon, Account, PokemonType, Type)
+            .with_entities(Pokemon.id, Pokemon.name, Account.username.label("creator"), func.group_concat(Type.name).label("types"), Pokemon.image)
+            .join(Account)
+            .join(PokemonType)
+            .join(Type)
+            .filter(Account.id == account_id)
+            .group_by(Pokemon.name).all())
 
 def validate_data(data):
     if not data or not data.get("types") or not data.get("name") or not data.get("image"):
@@ -44,5 +65,39 @@ def add_pokemon(types, name, image, username):
     
     db.session.add_all(pokemonType)
     db.session.commit()
+
+def is_created_by_user(pokemon_id, username):
+    account_id = Account.query.filter_by(username=username).first().id
+    pokemon_account_id = Pokemon.query.filter_by(id=pokemon_id).first().account_id
+
+    return True if account_id == pokemon_account_id else False
+
+def update_pokemon(types, name, image, pokemon_id): 
+    for type in types:
+        if not valid_type(type):
+            abort(400, type, "not found")
+
+    updated_pokemon = Pokemon.query.filter_by(id=pokemon_id).first()
+    updated_pokemon.name = name
+    updated_pokemon.image = image
+    db.session.commit()
     
-    return jsonify({"message": "pokemon successfully added!"})
+    pokemon_types = PokemonType.query.filter_by(pokemon_id=pokemon_id).all()
+
+    for pokemon_type in pokemon_types:
+        db.session.delete(pokemon_type)
+    db.session.commit()
+
+    pokemonType = []
+    for type in types:
+        type_id = Type.query.filter_by(name=type).first().id
+        pokemonType.append(PokemonType(pokemon_id=updated_pokemon.id, type_id=type_id))
+    
+    db.session.add_all(pokemonType)
+    db.session.commit()
+
+
+def delete_pokemon(pokemon_id):
+    delete_pokemon = Pokemon.query.filter_by(id=pokemon_id).first()
+    db.session.delete(delete_pokemon)
+    db.session.commit()
