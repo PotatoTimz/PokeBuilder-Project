c6cd7ce1cd61e935d9a4c641436c4df4474ec3f4
Andre Alix <andrealix2698@gmail.com>
2024-10-26 10:00:15 -0400

    added register page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/login";
 import { UserProvider } from "./context/userAuth";
+import Register from "./components/register";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <UserProvider>
         <Routes>
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </UserProvider>
     </>
