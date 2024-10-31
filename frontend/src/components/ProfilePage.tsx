964ab81b83365ff0acdffef13d8fe01cddadfa28
Andre Alix <andrealix2698@gmail.com>
2024-10-31 11:16:58 -0400

    minor changes

--- frontend/src/components/ProfilePage.tsx
+++ frontend/src/components/ProfilePage.tsx
@@ -4,7 +4,7 @@ import { SimplePokemonData } from "../interfaces/PokemonInterfaces";
 import { UserContext } from "../context/UserAuth";
 
 function ProfilePage() {
-  const { axiosFetch } = useContext(UserContext);
+  const { axiosFetch, username } = useContext(UserContext);
   const [userPokemon, setUserPokemon] = useState<SimplePokemonData[] | null>(
     null
   );
@@ -21,11 +21,27 @@ function ProfilePage() {
 
   return (
     <>
-      <div>user page</div>
-      {userPokemon?.map((pokemon, index) => {
-        console.log(pokemon);
-        return <div key={index}>{pokemon.pokemon_name}</div>;
-      })}
+      <div className="container-fluid">
+        <div id="userPage" className="row my-5 mx-2 justify-content-center">
+          <div className="col-lg-10 col-sm-16">
+            <div className="row bg-primary">
+              <div className="col-lg-3 col-sm px-3 py-5 m-2 userPageSection">
+                <div className="fs-4 fw-bold mb-4">{username}'s Profile</div>
+                <div className="fs-5 fw-bold text-decoration-underline mb-2">
+                  User Statistics:
+                </div>
+                <div className="fs-6 mb-1">Total Pokemon Created: </div>
+                <div className="fs-6">Total Moves Created: </div>
+              </div>
+              <div className="col px-3 py-5 m-2 userPageSection">test</div>
+            </div>
+          </div>
+        </div>
+        {userPokemon?.map((pokemon, index) => {
+          console.log(pokemon);
+          return <div key={index}>{pokemon.pokemon_name}</div>;
+        })}
+      </div>
     </>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./assets/PokemonCard.scss";
 import "./assets/MoveTable.scss";
+import "./assets/UserPage.scss";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
